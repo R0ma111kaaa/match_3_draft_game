86571842fda1cb3daa64df764be14c4acf9b1c23
Evgeny Bazarov <roma111kaaa@mail.ru>
2025-04-01 00:59:22 +0600

    closed tiles can move now

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: avoid_print
+
 import 'dart:math';
 
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
@@ -21,6 +24,8 @@ class Constants {
   static int columnCount = 6;
   static int spaceBetweenTiles = 10;
   static double fieldPadding = 10;
+  static double switchDuration = 0.4;
+  static double resizeDuration = 0.4;
 }
 
 class MyGame extends FlameGame {
@@ -28,29 +33,88 @@ class MyGame extends FlameGame {
 
   @override
   Future<void> onLoad() async {
-    children.register<GridComponent>();
     camera.viewfinder.anchor = Anchor.topLeft;
   }
 
-  // var pair = {};
-  // void addTileToPair(int row, int column) {
-  //   if (pair.containsKey("first")) {
-  //     pair["second"] = [row, column];
-  //     switchPair(pair);
-  //   } else {
-  //     pair["first"] = [row, column];
-  //   }
-  // }
+  var pair = [];
+  void addTileToPair(int row, int column) {
+    pair.add([row, column]);
+    if (pair.length == 2) {
+      switchPair();
+    }
+  }
 
-  // void switchPair(pair) {
-  //   GridComponent grid = children.query<GridComponent>().first;
-  //   var first = pair["first"];
-  //   var second = pair["second"];
-  //   var help = grid.tiles[first[0]][first[1]];
-  //   grid.tiles[first[0]][first[1]] = grid.tiles[second[0]][second[1]];
-  //   grid.tiles[second[0]][second[1]] = help;
-  //   this.pair = {};
-  // }
+  void switchPair() {
+    final grid = world.children.whereType<GridComponent>().first;
+
+    var first = pair[0];
+    var second = pair[1];
+
+    var tile1 = grid.tiles[first[0]][first[1]];
+    var tile2 = grid.tiles[second[0]][second[1]];
+
+    // Запоминаем их абсолютные позиции
+    Vector2 pos1 = tile1.getPosition();
+    Vector2 pos2 = tile2.getPosition();
+
+    List<List<int>> filledBlocks = getFilledBlocks();
+    print("empty list: ${filledBlocks.isEmpty}");
+
+    // Перемещаем тайлы
+    tile1.add(
+      SequenceEffect([
+        MoveToEffect(
+          pos2,
+          EffectController(
+            duration: Constants.switchDuration,
+            alternate: filledBlocks.isEmpty ? true : false,
+          ),
+        ),
+        SizeEffect.to(
+          Vector2.all(tile2.tileSize),
+          EffectController(duration: Constants.resizeDuration),
+        ),
+      ]),
+    );
+
+    tile2.add(
+      SequenceEffect([
+        MoveToEffect(
+          pos1,
+          EffectController(
+            duration: Constants.switchDuration,
+            alternate: filledBlocks.isEmpty ? true : false,
+          ),
+        ),
+        SizeEffect.to(
+          Vector2.all(tile2.tileSize),
+          EffectController(duration: Constants.resizeDuration),
+        ),
+      ]),
+    );
+
+    pair = [];
+  }
+
+  void resetPair() {
+    final grid = world.children.whereType<GridComponent>().first;
+    if (pair.isNotEmpty) {
+      var cords = pair[0];
+      var tile = grid.tiles[cords[0]][cords[1]];
+      tile.add(
+        SizeEffect.to(
+          Vector2.all(tile.tileSize),
+          EffectController(duration: Constants.resizeDuration),
+        ),
+      );
+    }
+    pair = [];
+  }
+
+  // Возвращает список удаляемых блоков
+  List<List<int>> getFilledBlocks() {
+    return [];
+  }
 }
 
 class MyWorld extends World with HasGameRef<MyGame> {
@@ -82,7 +146,7 @@ class GridComponent extends PositionComponent {
   int rowCount;
   int columnCount;
   double tileSize;
-  dynamic tiles;
+  List<List<dynamic>> tiles = [];
 
   Random random = Random();
 
@@ -95,7 +159,7 @@ class GridComponent extends PositionComponent {
   @override
   void onLoad() async {
     super.anchor = Anchor.center;
-    List<List<dynamic>> tiles = List<List>.generate(
+    tiles = List<List>.generate(
       rowCount,
       (i) => List<TileComponent>.generate(columnCount, (j) {
         TileComponent tile = TileComponent(
@@ -108,7 +172,6 @@ class GridComponent extends PositionComponent {
         return tile;
       }),
     );
-    this.tiles = tiles;
   }
 }
 
@@ -118,6 +181,7 @@ class TileComponent extends RectangleComponent
   int column;
   double tileSize;
   Random random;
+  bool isTapped = false;
 
   TileComponent({
     required this.row,
@@ -126,21 +190,50 @@ class TileComponent extends RectangleComponent
     required this.random,
   });
 
+  Vector2 getPosition() => position;
+
   @override
   void onLoad() {
+    anchor = Anchor.center;
     size = Vector2.all(tileSize);
     position = Vector2(
-      column * (tileSize + Constants.spaceBetweenTiles),
-      row * (tileSize + Constants.spaceBetweenTiles),
+      column * (tileSize + Constants.spaceBetweenTiles) + tileSize / 2,
+      row * (tileSize + Constants.spaceBetweenTiles) + tileSize / 2,
     );
     paint =
         Paint()..color = MyColors.colorList[random.nextInt(MyColors.lenght)];
   }
 
-  // @override
-  // void onTapDown(TapDownEvent event) {
-  //   gameRef.addTileToPair(row, column);
-  // }
+  @override
+  void onTapDown(TapDownEvent event) {
+    var pair = gameRef.pair;
+    if (!isTapped) {
+      if (pair.isNotEmpty) {
+        int verticalDistance = (pair[0][0] - row).abs();
+        int horisontalDistance = (pair[0][1] - column).abs();
+        if (!(horisontalDistance == 0 && verticalDistance == 1 ||
+                verticalDistance == 0 && horisontalDistance == 1) ||
+            pair[0] == [row, column]) {
+          gameRef.resetPair();
+          return;
+        }
+      }
+      isTapped = true;
+      add(
+        SizeEffect.to(
+          Vector2.all(tileSize * 0.9),
+          EffectController(
+            duration:
+                gameRef.pair.isNotEmpty
+                    ? Constants.resizeDuration / 2
+                    : Constants.resizeDuration,
+            onMax: () => isTapped = false,
+          ),
+        ),
+      );
+      gameRef.addTileToPair(row, column);
+    }
+  }
 }
 
 void main() {
